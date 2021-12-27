0e6d2231d804c03e037fd745c36ece7251f94c07
Pontus Knutsson <ponta45@hotmail.com>
2021-12-27 01:51:26 +0100

    Testing fix for lambda error

--- padelshop/netlify/functions/airtable_function.js
+++ padelshop/netlify/functions/airtable_function.js
@@ -7,27 +7,59 @@ const Airtable = require('airtable');
 
 
 const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base('appN7I9DtKRH4S7er');
-// const base = Airtable.Base("appN7I9DtKRH4S7er");
 
 exports.handler = function(event, context, callback) {
-    base('Table 1').select({
-        // Selecting the first 3 records in Grid view:
-        maxRecords: 3,
-        view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
+    const allRecords = []
+    base('Table 1')
+      .select({
+        maxRecords: 100,
+        view: 'all'
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          records.forEach(function(record) {
+            allRecords.push(record)
+          })
+          fetchNextPage()
+        },
+        function done(err) {
+          if (err) {
+            callback(err)
+          } else {
+            const body = JSON.stringify({ records: allRecords })
+            const response = {
+              statusCode: 200,
+              body: body,
+              headers: {
+                'content-type': 'application/json',
+                'cache-control': 'Cache-Control: max-age=300, public'
+              }
+            }
+            callback(null, response)
+          }
+        }
+      )
+  }
+
+// exports.handler = function(event, context, callback) {
+//     base('Table 1').select({
+//         // Selecting the first 3 records in Grid view:
+//         maxRecords: 3,
+//         view: "Grid view"
+//     }).eachPage(function page(records, fetchNextPage) {
+//         // This function (`page`) will get called for each page of records.
     
-        records.forEach(function(record) {
-            console.log('Retrieved', record.get('RacketId'));
-            console.log("Retrieved", record.get("RacketName"))
-        });
+//         records.forEach(function(record) {
+//             console.log('Retrieved', record.get('RacketId'));
+//             console.log("Retrieved", record.get("RacketName"))
+//         });
     
-        // To fetch the next page of records, call `fetchNextPage`.
-        // If there are more records, `page` will get called again.
-        // If there are no more records, `done` will get called.
-        fetchNextPage();
+//         // To fetch the next page of records, call `fetchNextPage`.
+//         // If there are more records, `page` will get called again.
+//         // If there are no more records, `done` will get called.
+//         fetchNextPage();
     
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-    });
-}
+//     }, function done(err) {
+//         if (err) { console.error(err); return; }
+//     });
+// }
