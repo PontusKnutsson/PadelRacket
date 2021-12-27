f7c6d53705ccc6202582a2c2a8d8cad5612e9684
Pontus Knutsson <ponta45@hotmail.com>
2021-12-27 01:00:52 +0100

    First try with Netlify function and call to Airtabel

---
+++ padelshop/netlify/functions/airtable_function.js
@@ -0,0 +1,31 @@
+const Airtable = require('airtable');
+
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+
+const base = Airtable.Base("appN7I9DtKRH4S7er");
+
+exports.handler = function(event, context, callback) {
+    base('Table 1').select({
+        // Selecting the first 3 records in Grid view:
+        maxRecords: 3,
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+    
+        records.forEach(function(record) {
+            console.log('Retrieved', record.get('RacketId'));
+            console.log("Retrieved", record.get("RacketName"))
+        });
+    
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+    
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+    });
+}
--- padelshop/src/views/Home.vue
+++ padelshop/src/views/Home.vue
@@ -8,11 +8,22 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import axios from 'axios'
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  setup() {
+    const axiosInstance = axios.create({
+      baseURL: "/netlify/functions",
+      timeout: 10000
+    });
+
+    axiosInstance.get("airtable_function").then(resp => {
+      console.log(resp.data);
+    });
   },
 });
 </script>
