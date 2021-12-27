687ce6daf20a572e67853c8f106f9a91901baa19
Pontus Knutsson <ponta45@hotmail.com>
2021-12-28 00:04:18 +0100

    Added card for rackets

---
+++ padelshop/src/components/ItemCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="card">
+        <img :src="props.imageSrc" :alt="props.imageAltText">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        imageSrc: {
+            type: String,
+            required: true
+        },
+        imageAltText: {
+            type: String,
+            required: true
+        }
+    },
+    setup(props) {
+        
+        return {
+            props
+        }
+    },
+})
+</script>
+
+<style lang="scss" scoped>
+    .card {
+        height: 100px;
+        width: 50px;
+
+        img {
+            width: 100%;
+            height: 70px;
+        }
+    }
+</style>
--- padelshop/src/views/Home.vue
+++ padelshop/src/views/Home.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="home">
-    <div v-for="(racket, index) in airtable.rackets" :key="index">
+    <!-- <div v-for="(racket, index) in airtable.rackets" :key="index">
       {{racket.fields.RacketName}}
       {{racket.fields.Price}}
       {{racket.fields.ImgContent}}
-    </div>
-    <img alt="Racket logo" src="https://cocky-kowalevski-3f8a74.netlify.app/head-graphene-360-alpha-pro.jpg">
+    </div> -->
+    <ItemCard :imageSrc="racket.fields.ImgConent" :imageAltText="racket.fields.RacketName" v-for="(racket, index) in airtable.rackets" :key="index">
+      {{racket.fields.RacketName}}<br />
+      {{racket.fields.Price}}
+    </ItemCard>
+    <!-- <img alt="Racket logo" src="https://cocky-kowalevski-3f8a74.netlify.app/head-graphene-360-alpha-pro.jpg"> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive } from 'vue';
+import ItemCard from "@/components/ItemCard.vue"
+
 import { GetPadelRacket } from "@/API/AirTableAPICaller"
 import { AirTableRecord } from "@/Models/AirTableRecord"
 import PadelRacket from "@/Models/AirTablePadelRacket"
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    ItemCard
+  },
   setup() {
     const airtable = reactive({
       rackets: [] as AirTableRecord<PadelRacket>[]
