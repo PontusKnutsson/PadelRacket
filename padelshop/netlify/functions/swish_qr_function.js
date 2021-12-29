d0f9fd10fc86b93cc7037d9dfeb4638176a5bd17
Pontus Knutsson <ponta45@hotmail.com>
2021-12-29 23:42:28 +0100

    logging response to see output in lambda function

--- padelshop/netlify/functions/swish_qr_function.js
+++ padelshop/netlify/functions/swish_qr_function.js
@@ -23,6 +23,8 @@ exports.handler =  function(event, context, callback) {
         'content-type': 'application/json'
       }
     }).then(resp => {
+        console.log("Resp.data: " + resp.data);
+        console.log("Resp from buffer: " + Buffer.from(resp.data, 'binary').toString())
         const body = Buffer.from(resp.data, 'binary').toString('base64')
         const response = {
             statusCode: 200,
