b744fabb3718d4e3d79df0c3812a05b8e9c2d3bd
Pontus Knutsson <ponta45@hotmail.com>
2021-12-29 23:05:15 +0100

    More logging

--- padelshop/netlify/functions/swish_qr_function.js
+++ padelshop/netlify/functions/swish_qr_function.js
@@ -23,7 +23,7 @@ exports.handler =  function(event, context, callback) {
         'content-type': 'application/json'
       }
     }).then(resp => {
-        console.log("Please work...: " + resp.content)
+        console.log("Please work...: " + resp.Content-Type)
         const body = resp;
         const response = {
             statusCode: 200,
