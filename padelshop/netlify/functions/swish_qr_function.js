d0596b2c586193626252af4a3ddfe6613224b968
Pontus Knutsson <ponta45@hotmail.com>
2021-12-29 01:23:34 +0100

    Added logging to swish to find problem

--- padelshop/netlify/functions/swish_qr_function.js
+++ padelshop/netlify/functions/swish_qr_function.js
@@ -23,6 +23,7 @@ exports.handler =  function(event, context, callback) {
         'content-type': 'application/json'
       }
     }).then(resp => {
+        console.log("Please work...: " + resp)
         const body = resp;
         const response = {
             statusCode: 200,
