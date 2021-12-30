80da8fc330a5ba3baa0a2eccdd08e67d48351a6b
Pontus Knutsson <ponta45@hotmail.com>
2021-12-30 01:48:12 +0100

    changed to extend blob from buffer

--- padelshop/netlify/functions/swish_qr_function.js
+++ padelshop/netlify/functions/swish_qr_function.js
@@ -54,7 +54,7 @@ exports.handler = function(event, context, callback) {
     }).then(resp => {
         // console.log("Resp.data: " + resp.data);
         // console.log("Resp from buffer: " + Buffer.from(resp.data, 'binary').toString())
-        const body = new Blob([resp.data]);
+        const body = new buffer.Blob([resp.data]);
         const response = {
             statusCode: 200,
             body: body,
